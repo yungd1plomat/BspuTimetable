cb8848dd60e0ee4f84122ffadb332d4cfe87638e
yungd1plomat <aynur.musin.06@mail.ru>
2023-11-29 18:10:54 +0500

    add startup configuration

--- src/VkBot/Program.cs
+++ src/VkBot/Program.cs
@@ -1,27 +1,19 @@
+using VkNet;
+
 namespace VkBot
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-
-            var app = builder.Build();
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ src/VkBot/Startup.cs
@@ -0,0 +1,45 @@
+﻿using VkBot.Abstractions;
+using VkBot.Commands;
+using VkBot.Services;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model;
+
+namespace VkBot
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration["CONNECTION_STRING"] ?? throw new InvalidOperationException("connection string not found");
+            var token = Configuration["ACCESS_TOKEN"] ?? throw new InvalidOperationException("access token not found");
+            services.AddScoped<IVkApi, VkApi>(provider =>
+            {
+                var api = new VkApi();
+                api.Authorize(new ApiAuthParams()
+                {
+                    AccessToken = token,
+                });
+                return api;
+            });
+            services.AddControllers().AddNewtonsoftJson();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
